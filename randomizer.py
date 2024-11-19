76a9e2fda888d8576e82b6dd0f05f1c1c93c7372
Wetal <margenshteynv@gmail.com>
2024-11-20 01:08:04 +0300

    Initial commit

---
+++ randomizer.py
@@ -0,0 +1,11 @@
+def main():
+    from groups import group_1, group_2, group_3
+    dict = input("Введите список группы\n")
+    number_of_groups = input("Введите колличество групп\n")
+    print(f'Группа 1:{group_1}')
+    print(f'Группа 2:{group_2}')
+    print(f'Группа 3:{group_3}')
+    print(f'Группа 4:{group_4}')
+    print(f'Группа 5:{group_5}')
+
+main()
